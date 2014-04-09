5287f7c3fbc85eb7c47ce27d9b2404317e87c609
Finn <thefinn93@thefinn93.com>
2014-04-08 19:03:53 -0700

    Fixed pingall

--- pingall.sh
+++ pingall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while read host; do
     echo -e "$host ... \c"
-    ssh -n $host "fping -e $1" | cut -d'(' -f 2 | sed 's/)//'
+    ssh -n $host "fping -e $@" | cut -d'(' -f 2 | sed 's/)//'
 done < hosts.txt
